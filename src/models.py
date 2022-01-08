ce55124cb51c1983342a85c4048bb939ce94f495
Fabo1808 <fabianjga@gmail.com>
2022-01-08 16:42:23 +0000

    version final

--- src/models.py
+++ src/models.py
@@ -1,64 +1,65 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 from eralchemy import render_er
-
-from example import Vehicle
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
-    user_name = Column(String(30), nullable=False, unique=True)
-    email = Column(String(80), nullable=False, unique=True)
-    favorite_id = Column(Integer, ForeignKey('favorite.id'))
-    def verifyLogin(self):
-        """verify login"""
-    
-    def addToFavorite(self):
-        """Add to Favorite """
-
-    def removeFavorite(self):
-        """remove Favorite"""
+    __tablename__='user'
+    id=Column(Integer, primary_key=True)
+    user_name=Column(String(40), unique = True)
+    first_name=Column(String(20), nullable=False)
+    last_name=Column(String(20), nullable=False)
+    email=Column(String (50), unique=True)
+    sign_in_date=Column(DateTime(), nullable=True)
+    favorites= relationship('favorites', backref='user', uselist=True)
+    favorite_id=Column(Integer, ForeignKey('favorite.id'), nullable=False)
 
 class Favorite(Base):
-    __tablename__ = 'favorite'
-    id = Column(Integer, primary_key=True)
-    character_id = Column(Integer, ForeignKey('character.id'), nullable=True)
-    Vehicle_id = Column(Integer, ForeignKey('vehicle.id'), nullable=True)
-    planet_id = Column(Integer, ForeignKey('planet.id'), nullable=True)
+    __tablename__= 'favorite'
+    id=Column(Integer, primary_key=True)
+    character_id=Column(Integer, ForeignKey('character.id'), nullable=False)
+    planet_id=Column(Integer, ForeignKey('planet.id'), nullable=False)
+    vehicle_id=Column(Integer, ForeignKey('vehicle.id'), nullable=False)
+    favorites=relationship('favorites', backref='user', uselist=True)
 
 class Character(Base):
     __tablename__= 'character'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(30), nullable=False, unique=True)
-    birth_day = Column(Integer, nullable=False)
-    gender = Column(String(80), nullable=False)
-    height = Column(Integer, nullable=False)
-    skin_color = Column(String(80), nullable=False)
+    id=Column(Integer, primary_key=True)
+    name=Column(String(50), nullable=False)
+    race=Column(String(30), nullable=False)
+    gender=Column(String(30), nullable=False)
+    skills=Column(String(200), nullable=False)
+    personality=Column(String(200), nullable=False)
+    
+class Planet(Base):
+    __tablename__= 'planet'
+    id=Column(Integer, primary_key=True)
+    name=Column(String(40), nullable=False)
+    type=Column(String(30), nullable=False)
+    constellation=Column(String(50), nullable=False)
+    composition=Column(String(70), nullable=False)
+    dominant_race=Column(String(40), nullable=False)
+    government_system=Column(String(40), nullable=False)
+
 
 class Vehicle(Base):
-    __tablename__ = 'vehicle'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False, unique=True)
-    crew = Column(String(80), nullable=False)
-    passenger = Column(String(80), nullable=False)
-    vehicle_class = Column(String(80), nullable=False)
-    model = Column(String(80), nullable=False)
-    manufacturer = Column(String(80), nullable=False)
+    __tablename__='vehicle'
+    id=Column(Integer, primary_key=True)
+    name=Column(String(50), nullable=False)
+    type=Column(String(200), nullable=False)
+    constructor=Column(String(30), nullable=False)
+    functionality=Column(String(100), nullable=False)
+    length=Column(String(30), nullable=False)
+    
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False, unique=True)
-    climate = Column(String(80), nullable=False)
-    terrain = Column(String(80), nullable=False)
-    gravity = Column(String (80), nullable=False) 
-    diameter = Column(Integer, nullable=False)
-    population = Column(Integer, nullable=False)  
 
+
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
